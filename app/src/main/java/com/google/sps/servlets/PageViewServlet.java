916a7d7e06503629aa394d8ffa18fb8e46641b70
Sheneska Williams <74882676+sheneska@users.noreply.github.com>
2021-04-12 12:21:38 -0400

    Mvp1 (#25)
    * Ran code on live server, also added our bios to html file in the contect section.
    
    * Created multiple servers to host the datastore library, also deployed to live server successfully.

---
+++ app/src/main/java/com/google/sps/servlets/PageViewServlet.java
@@ -0,0 +1,25 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+
+/** Servlet that returns HTML that contains the page view count. */
+@WebServlet("/page-views") //ADD WEBSITE LINK
+public class PageViewServlet extends HttpServlet {
+
+  private int pageViews = 0;
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    pageViews++;
+
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Page Views</h1>");
+    response.getWriter().println("<p>This page has been viewed " + pageViews + " times.</p>");
+    System.out.println("Hello World");
+  }
+}
